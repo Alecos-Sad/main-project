c3126f8d9bef88cf71275f97f16e607c28cac78c
Alecos-Sad <alecos100@gmail.com>
2025-03-05 19:03:25 +0300

    redis java

---
+++ redis-java/src/test/java/by/sadovnick/redisjava/RedisJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sadovnick.redisjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
